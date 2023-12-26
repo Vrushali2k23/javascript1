12d189df64c5ae4011943481eec37914ac40b9c3
Vrushali2k23 <bhimanavaruvrushali@gmail.com>
2023-12-26 23:03:46 +0530

    01_assign.html
    Assignment done

---
+++ 01_Assign.js
@@ -0,0 +1,29 @@
+console.log("Hello  JS,I am Vrushali");
+var myName;
+
+myName = "Vrushali";
+console.log("First Name:",myName);
+var Age ="32";
+
+Age = "32";
+console.log("Age:",32);
+
+var MyCompleteName ;
+MyCompleteName= "Vrushali R Bhiamanavaru";
+console.log("My Complete Name:",MyCompleteName);
+
+var Address ;
+Address= "Solapur";
+console.log("Address:",Pincode);
+
+var Pincode ;
+Pincode= "413004";
+console.log("Pincode:",Pincode);
+
+var Gender ;
+Gender = "Female";
+console.log("Gender:",Gender);
+
+var Married  ;
+Married = "Yes";
+console.log("Married:",Married);
